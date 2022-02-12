27554adcbf06f64513e223c40386766d61b1e929
Felipe Paulino <felipeoliveirapaulino2@gmail.com>
2022-02-12 15:59:42 -0300

    Add repository and register as a service. Create DbContext

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using CommandAPI.Data;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace CommandAPI.Controllers
 {
@@ -6,10 +8,29 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly ICommandAPIRepo _repository;
+
+        public CommandsController(ICommandAPIRepo repository){
+            _repository = repository;
+        }
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
         {
-            return new string[] {"this", "is", "hard", "coded"};
+            var commandItems = _repository.GetAllCommands();
+
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var command = _repository.GetCommandById(id);
+            if (command == null){
+                return NotFound();
+            }
+
+            return Ok(command);
         }
     }
 }
